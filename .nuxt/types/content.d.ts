8de6b10ef4f1db698ce6558b23093e047d68ce84
SalahEldin Fikri <71356170+SalahEldinFikri@users.noreply.github.com>
2024-09-21 12:43:50 +0300

    Add files via upload

---
+++ .nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('D:/webProjects/side-projects/mr_matrix/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('D:/webProjects/side-projects/mr_matrix/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
